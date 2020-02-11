c9c9e879ed8da5fa93ee4b52a144fc4c19a9c1bd
[Students] Ralfs Dabolins <83DRDabolins@rvt.lv>
2020-02-11 08:14:49 +0200

    izmainiju main un fail proc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -17,6 +18,46 @@ def about_html():
 @app.route('/contact')
 def contact():
   return render_template('contact.html', phone="12345678")
+
+@app.route('/params')
+def params():
+  args = request.args
+  for key, value in args.items():
+    print(f"(key):(value)")
+  return args
+
+@app.route('/params_table')
+def params_table():
+  args = request.args
+  return render_template('params_table.html', args = args)
+
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
+
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  content_type = request.content_type
+  if content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"add line {contentJSON['data']} to file."
+  else:
+    return f"Content type {content_type} is not supported"
+
+@app.route('/file', methods = ['GET', 'POST'])
+def workfile():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} is not supported"
 
 if __name__ == '__main__':
   app.run(threaded=True, port=5020, debug=True)
